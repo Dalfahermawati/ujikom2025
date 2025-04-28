8c8f5d49353b0ba074ee439e801b1f5d437483fb
Dalfahermawati <dalfahermawati770@gmail.com>
2025-04-28 09:17:15 +0700

    menambah link firebase main js

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
-import { log } from './logger'
-
-log('Import works!')
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyAjxjGgc1_HHBUGoXM1kFq4aXiV--plwZE",
+  authDomain: "pasarcemerlang-11fa3.firebaseapp.com",
+  projectId: "pasarcemerlang-11fa3",
+  storageBucket: "pasarcemerlang-11fa3.appspot.com",
+  messagingSenderId: "390685080124",
+  appId: "1:390685080124:web:6a69ed5fd39c3fc21da139",
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export async function ambilDaftarData() {
+  const refDokumen = collection(db, "todolist1");
+  const kueri = query(refDokumen, orderBy("nama"));
+  const cuplikanKueri = await getDocs(kueri);
+  let hasil = [];
+  cuplikanKueri.forEach((dok) => {
+    hasil.push({
+      id: dok.id,
+      nama: dok.data().nama,
+      prioritas: dok.data().prioritas,
+      status: dok.data().status,
+      tanggal: dok.data().tanggal,
+    });
+  });
+  return hasil;
+}
